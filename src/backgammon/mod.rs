8cdd6a3a269777286d10064a0a40616b289ad0c4
alibasaran <alibasaran2010@gmail.com>
2023-08-03 16:56:20 +0300

    Skeleton

---
+++ src/backgammon/mod.rs
@@ -0,0 +1,16 @@
+pub struct Backgammon {
+    // board:: 24 poz
+    // 15'er tas
+    // kirik taslar [beyaz, siyah]
+    // toplanmis taslar [beyaz, siyah]
+    // hamle sirasi
+}
+
+impl Backgammon {
+    // new // Sinan
+    // get_initial_state returns board // Sinan
+    // get_next_state(state, action, player) // Sinan
+    // get_valid_moves(die, state) // Ali
+    // check_win // Ali
+}
+
