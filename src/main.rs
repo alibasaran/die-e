63aa957e7705eedaebbb9d669d0534194f3986ba
alibasaran <alibasaran2010@gmail.com>
2023-09-27 12:38:29 +0300

    Implemented Train command
    The goal is the use it for training a model directly with previously gathered self-play data

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::{
     collections::HashMap,
-    time::Duration, path::{Path, PathBuf},
+    time::Duration, path::{Path, PathBuf}, fs, io,
 };
 
 use config::Config;
 use die_e::{
     backgammon::backgammon_logic::Backgammon,
     mcts::alpha_mcts::TimeLogger,
-    constants::DEVICE, alphazero::alphazero::{AlphaZero, AlphaZeroConfig, AZ_CONFIG_KEYS},
+    constants::DEVICE, alphazero::alphazero::{AlphaZero, AlphaZeroConfig},
 };
 use itertools::Itertools;
-use tch::Tensor;
+use tch::{Tensor, nn::VarStore};
 
 use clap::{Parser, Subcommand};
 
@@ -44,6 +44,9 @@ enum Commands {
         // Path of the model to train
         #[arg(short, long)]
         model_path: Option<PathBuf>,
+        // Path to output model after training
+        #[arg(short, long)]
+        model_save_path: Option<PathBuf>,
         // The run id for the data, uses all data under run id to train
         #[arg(short, long)]
         run_id: Option<String>,
@@ -63,7 +66,7 @@ fn main() {
         PathBuf::from("./config")
     );
     let builder = Config::builder()
-        .add_source(config::File::new(config_path.to_str().unwrap(), config::FileFormat::Json));
+        .add_source(config::File::new(config_path.to_str().unwrap(), config::FileFormat::Toml));
 
     let config = match builder.build() {
         Ok(config) => config,
@@ -94,6 +97,58 @@ fn main() {
             az.learn_parallel();
         },
         Commands::Play {  } => todo!(),
-        Commands::Train { model_path, run_id, learn, self_play } => todo!(),
+        Commands::Train { model_path, model_save_path,  run_id, learn, self_play } => {
+            println!("Starting training process");
+            // Load training data
+            let data_path = match (run_id, learn, self_play) {
+                (None, None, None) => String::from("./data"),
+                (Some(id), None, None) => format!("./data/run-{}", id),
+                (Some(id), Some(learn_id), None) => format!("./data/run-{}/lrn-{}", id, learn_id),
+                (Some(id), Some(learn_id), Some(sp_id)) => format!("./data/run-{}/lrn-{}/sp-{}", id, learn_id, sp_id),
+                _ => panic!("the request for the training data is incorrect, run die-e learn --help for more info")
+            };
+            let data_path = Path::new(&data_path);
+            if !data_path.exists() {
+                panic!("[TRAIN] the specified path {} does not exist!", data_path.to_str().unwrap());
+            }
+            let mut paths_to_load = vec![];
+            let _ = get_all_paths_rec(data_path, &mut paths_to_load);
+            let mut training_data = paths_to_load.iter().flat_map(|p| 
+                AlphaZero::load_training_data(p.as_path())
+            ).collect_vec();
+
+            println!("Total memory fragments: {}", &training_data.len());
+            
+            // Create AZ instance for training
+            let mut az_config = match AlphaZeroConfig::from_config(&config){
+                Ok(config) => config,
+                Err(e) => panic!("Unable to load AlphaZero config, {}", e),
+            };
+            az_config.model_path = model_path;
+            let mut az = AlphaZero::new(az_config);
+
+            // Train and save model
+            az.train(&mut training_data);
+            let final_out_path = model_save_path.unwrap_or(Path::new("./models/trained_model.ot").to_path_buf());
+            match az.model.vs.save(&final_out_path) {
+                Ok(_) => println!("Trained model saved successfully, saved to {}", final_out_path.to_str().unwrap()),
+                Err(e) => panic!("unable to save trained model {}", e),
+            }
+        },
     }
+}
+
+fn get_all_paths_rec(dir: &Path, res: &mut Vec<PathBuf>) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.file_name().unwrap().to_str().unwrap().contains("sp") {
+                res.push(path);
+            } else {
+                get_all_paths_rec(&path, res)?;
+            }
+        }
+    }
+    Ok(())
 }
